926afd1af992bfbc3412987e09d8fe21fee749fd
Th1agoGuidi <thiagoguidi@outlook.com>
2024-03-26 04:54:14 -0300

    avatar changes

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function toggleMode() {
     const img = document.querySelector("#profile img");
 
     if (html.classList.contains("light")) {
-        img.setAttribute('src', './assets/avatar-light.png');
+        img.setAttribute('src', './assets/avatar-light.jpg');
         img.setAttribute('alt', "Foto de Thiago com óculos escuro (O Brilho está muito forte)");
     }
     else {
-        img.setAttribute('src', './assets/avatar.png');
+        img.setAttribute('src', './assets/avatar.jpg');
         img.setAttribute('alt', "Foto de Thiago");
     }
 }
